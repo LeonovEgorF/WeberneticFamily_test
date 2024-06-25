e4d77cbdfa7e9c749d0a285e72c25ab9d32d6bac
LeonovEgorF <egor1908608@gmail.com>
2024-06-26 00:27:31 +0300

    add modal signIn, adding user data collection logic

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,26 @@ import { initializeApp } from "firebase/app";
 import firebaseConfig from "./firebaseConfig";
 import "../style/style.scss";
 initializeApp(firebaseConfig);
+
+const btnOpenModal = document.querySelector(".btn");
+const btnCloseModal = document.querySelector(".modal__btn-close");
+const modal = document.querySelector(".modal");
+const form = document.forms.form;
+
+btnOpenModal.addEventListener("click", toggleModal);
+btnCloseModal.addEventListener("click", toggleModal);
+form.addEventListener("submit", sendForm);
+
+function toggleModal() {
+  modal.classList.toggle("none");
+}
+
+function sendForm(e) {
+  e.preventDefault();
+  const data = new FormData(form);
+  const infUser = {};
+  for (let [key, value] of data) {
+    infUser[key] = value;
+  }
+  console.log(infUser);
+}
